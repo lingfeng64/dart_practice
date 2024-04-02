a884ac0ad5bb702e7be927e2459f803ba1aa57f8
ling feng <2809926136@qq.com>
2024-04-02 08:18:21 +0000

    ma

--- var/vaa.dart
+++ var/vaa.dart
@@ -1,3 +1,11 @@
 void main() {
-  print('Hello, World!');
+  print('Hello, World!10086');
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
 }
